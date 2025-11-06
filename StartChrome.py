e0ba36cade41fa46f16512edd1d6641895a2f60a
prathamesh.khulpe <prathamesh1514@gmail.com>
2025-11-06 15:28:06 +0530

    this is test

--- StartChrome.py
+++ StartChrome.py
@@ -21,6 +21,6 @@ driver.find_element(By.ID, "nav-btn-practice").click()
 driver.find_element(By.ID,"login-email").send_keys("nkosi1@gmail.com")
 driver.find_element(By.ID,"login-password").send_keys("12345678")
 driver.find_element(By.ID, "login-submit").click()
-time.sleep(5)
-driver.find_element(By.ID, "practice-heading").is_displayed()
 time.sleep(10)
+driver.find_element(By.ID, "practice-heading").is_displayed()
+
