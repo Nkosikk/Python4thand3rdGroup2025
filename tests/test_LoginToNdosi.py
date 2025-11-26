45c417facf1888dbcfe5afaa720ecc2729564baa
MosRamTest <mosram2@gmail.com>
2025-11-26 19:05:13 +0200

    Refactored test and created setUp Class

--- tests/test_LoginToNdosi.py
+++ tests/test_LoginToNdosi.py
@@ -4,6 +4,7 @@ import pytest
 
 from Pages.homePage import HomePage
 from Pages.loginPage import LoginPage
+from utils.browserSetUp import setup_Browser
 from utils.readProperties_data import ReadConfig_data
 from utils.commonLogin import loginToNdosi
 
@@ -15,34 +16,14 @@ class Test_LoginToNdosi:
 
     @pytest.mark.dev
     def test_loginWithValidDetails(self, setup):
-        self.driver = setup
-        self.driver.get(self.dev_url)
-        self.driver.maximize_window()
-        # self.hp = HomePage(self.driver)
-        # self.hp.verifyNdosiHeading()
-        # self.hp.clickLearningMaterial()
-        # self.login=LoginPage(self.driver)
-        # self.login.enterEmail(self.username)
-        # self.login.enterPassword(self.password)
-        # self.login.clickLoginBtn()
-        loginToNdosi(self.driver,self.username,self.password)
+        self.driver = setup_Browser(setup)
+        loginToNdosi(self.driver, self.username, self.password)
 
         time.sleep(2)
 
         self.driver.quit()
+
     @pytest.mark.dev
     def test_loginWithInvalidDetails(self, setup):
-        self.driver = setup
-        self.driver.get(self.dev_url)
-        self.driver.maximize_window()
-        # self.hp = HomePage(self.driver)
-        # self.hp.verifyNdosiHeading()
-        # self.hp.clickLearningMaterial()
-        # self.login = LoginPage(self.driver)
-        # self.login.enterEmail(self.username)
-        # self.login.enterPassword(self.password+"invalid")
-        # self.login.clickLoginBtn()
-        loginToNdosi(self.driver, self.username, self.password+"invalid")
-
-
-
+        self.driver = setup_Browser(setup)
+        loginToNdosi(self.driver, self.username, self.password + "invalid")
--- utils/commonLogin.py
+++ utils/commonLogin.py
@@ -1,7 +1,13 @@
 from Pages.homePage import HomePage
 from Pages.loginPage import LoginPage
+from tests.conftest import setup
+from utils.readProperties_data import ReadConfig_data
+
+
+
 
 def loginToNdosi(driver,username,password):
+
     hp = HomePage(driver)
     hp.verifyNdosiHeading()
     hp.clickLearningMaterial()
@@ -9,5 +15,6 @@ def loginToNdosi(driver,username,password):
     login.enterEmail(username)
     login.enterPassword(password)
     login.clickLoginBtn()
+
 
 
